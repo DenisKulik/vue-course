fd3617f3abf9d5a8af00ad4375d373c6b9d234e3
Denis Kulik <kulikdenis1996x@gmail.com>
2024-05-26 18:04:06 +0700

    feat: add counter button

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,17 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
