5c6713dac9b67e5d3a5e29aaddfa66c2a06a9140
Denis Kulik <kulikdenis1996x@gmail.com>
2024-05-26 19:21:13 +0700

    feat: add getting meetup title by id

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
+const App = defineComponent({
+  name: 'App',
+  data: () => ({
+    currentMeetupId: 1,
+    meetupTitle: null,
+  }),
+  async mounted() {
+    this.fetchMeetupById(this.currentMeetupId);
+  },
+  methods: {
+    async fetchMeetupById(meetupId) {
+      try {
+        const response = await fetch(`${API_URL}/meetups/${meetupId}`);
+        const data = await response.json();
+        this.meetupTitle = data.title;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  watch: {
+    async currentMeetupId(newValue) {
+      this.fetchMeetupById(newValue);
+    },
+  },
+});
 
-// Требуется создать Vue приложение
+const app = createApp(App);
+app.mount('#app');
