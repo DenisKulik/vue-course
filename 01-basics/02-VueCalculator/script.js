6492dcd570d4ca76daa9ccd1b5999312bf385630
Denis Kulik <kulikdenis1996x@gmail.com>
2024-05-26 18:14:27 +0700

    feat: add calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data: () => ({
+    operator: null,
+    firstValue: 0,
+    secondValue: 0,
+  }),
+  computed: {
+    result() {
+      if (this.operator === 'sum') {
+        return this.firstValue + this.secondValue;
+      }
+      if (this.operator === 'subtract') {
+        return this.firstValue - this.secondValue;
+      }
+      if (this.operator === 'multiply') {
+        return this.firstValue * this.secondValue;
+      }
+      if (this.operator === 'divide') {
+        return this.firstValue / this.secondValue;
+      }
+      return 0;
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
